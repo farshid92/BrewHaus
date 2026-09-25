38756ace28420e6cc362be2a2f1ed7de5014d52d
farshid92 <farshidcheraghchian@gmail.com>
2026-09-25 11:22:24 +0300

    fix: polyfill globalThis.crypto for environments where it's not a default global

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,7 @@
+import { webcrypto } from 'node:crypto'
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto
+}
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
